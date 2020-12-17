899bb2ea22db391c06ed772a6f1e42ec2366eca5
SC <63264140+SC75@users.noreply.github.com>
2020-12-17 16:41:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,25 +15,15 @@ download('punkt')
 app = Flask(__name__)
 
 # Prometheus Monitoring
-graphs = {}
-graphs['c_index'] = Counter('app_index_access', 'Counter for main page')
-graphs['c_res'] = Counter('app_results_page', 'Counter for results page')
-graphs['g_inprogress'] = Gauge('app_page_access_in_progress', 'Visits in progress')
-graphs['g_last'] = Gauge('app_page_accesses_last_time_seconds', 'The last time results_page was accessed')
-graphs['l'] = Summary('app_latency_in_seconds', 'Time required for a request')
-graphs['h'] = Histogram('app_python_requests_duration_seconds', 'Histogram for the duration in seconds', buckets=(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2, 5, 7, 10))
-graphs['e'] = Counter('app_exceptions_total', 'How many times an exception has been issued')
+c_index = Counter('app_index_access', 'Counter for main page')
+c_res= Counter('app_results_page', 'Counter for results page')
+e = Counter('app_exceptions_total', 'How many times an exception has been issued')
+g_inprogress = Gauge('app_page_access_in_progress', 'Visits in progress')
+g_last = Gauge('app_page_accesses_last_time_seconds', 'The last time results_page was accessed')
+l = Summary('app_latency_in_seconds', 'Time required for a request')
+h = Histogram('app_python_requests_duration_seconds', 'Histogram for the duration in seconds', buckets=(0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2, 5, 7, 10))
 
 # Preprocessing 
-stop_words = stopwords.words('english')
-model = models.Word2Vec.load('word2vec.model')
-
-df = pd.read_csv('tweets.csv')
-df_text = pd.DataFrame()
-df_text['Text'] = df.text
-similarity_index = WmdSimilarity(df_text['Text'], model, num_best=20)
-
-
 def pre_processing_text(text):
     # lowercase
     text = text.lower()
@@ -54,11 +44,22 @@ def pre_processing_text(text):
     # stopwords
     return [word for word in word_tokenize(text) if word not in stop_words]
 
+# Word2Vec pretrained model
+stop_words = stopwords.words('english')
+model = models.Word2Vec.load('word2vec.model')
+
+# Dataframe creation and model implementation
+df = pd.read_csv('tweets.csv')
+df_text = pd.DataFrame()
+df.text = df.text.apply(pre_processing_text(df.text))
+df_text['Text'] = df.text
+similarity_index = WmdSimilarity(df_text['Text'], model, num_best=20)
+
 # Flask
 @app.route('/')
 def index():
-    graphs['c_index'].inc()
-    with graphs['e'].count_exceptions():
+    c_index.inc()
+    with e.count_exceptions():
         if random.random() < 0.2:
             raise Exception 
     return render_template('index.html')
@@ -66,9 +67,9 @@ def index():
 @app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
-        graphs['c_res'].inc()
-        graphs['g_inprogress'].inc()
-        graphs['c_index'].inc()
+        c_res.inc()
+        g_inprogress.inc()
+        c_index.inc()
         start = time.time()
         answer = request.form['Answer']
         clean_answer = pre_processing_text(answer)
@@ -80,19 +81,12 @@ def result():
             score_results.append(similar[i][1])
         score_results = ["{:.2f}".format(score) for score in score_results]
         end = time.time()
-        graphs['g_last'].set(end)
-        graphs['g_inprogress'].dec()
-        graphs['l'].observe(end - start)
-        graphs['h'].observe(end - start)
+        g_last.set(end)
+        g_inprogress.dec()
+        l.observe(end - start)
+        h.observe(end - start)
         return render_template('results.html', results=zip(text_results,score_results))
     return render_template('index.html')
-
-@app.route('/metrics')
-def requests_count():
-    res = []
-    for key,value in graphs.items():
-        res.append(prometheus_client.generate_latest(value))
-    return Response(res, mimetype="text/plain")
 
 if __name__ == "__main__":
     start_http_server(8010)
